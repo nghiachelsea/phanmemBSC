f5e62afeea45a2ec7ae7ab56fd24780669337533
nghiachelsea <nghiab1607006@student.ctu.edu.vn>
2020-08-19 15:04:07 +0700

    Nghia_Thu

--- model/chitietPDG/ctphieudanhgia.php
+++ model/chitietPDG/ctphieudanhgia.php
@@ -7,7 +7,7 @@
 		$sql = "INSERT INTO images (image, text) VALUES ('$image', '$text')";
 		mysqli_query($db, $sql);
 		if(move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
-			$msg = "Thành công!";
+			$msg = "Thành công";
 		} 
 		else{
 			$msg = "Thất bại";
@@ -28,17 +28,16 @@
 
 </head>
 <body>
-    <center><h1> Bảng Đánh Giá Các Tiêu Chí<h1></center>
-    <p> Mã Phiếu: </p>
+   <p> Mã Phiếu: </p>
     <p> Mã Chi Tiết PDG: </p>
     <p> Mã Trạm: </p>
     <p> Người đánh giá: <?php include("getND.php") ?></p>
     <a> Ngày Đánh Giá: </a> <a id="date"></a>
-<div id="toolbarP">
-    <div id="tbP2" style="">
-        <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-edit" plain="true" onclick="editP()">Thêm Ghi Chú</a>
-    </div>
-</div>
+
+<table id="dg"  >
+  
+    
+</table>
 <div id="dlgP" class="easyui-dialog" style="width:50%; height: 200%" data-options="closed:true,modal:true,border:'thin',buttons:'#dlgP-buttons'">
         <form id="fmP" method="post" action="ctphieudanhgia.php" enctype="multipart/form-data" novalidate style="margin:0;padding:10px 50px">
             <h3>Nhập thông tiêu chí</h3>
@@ -57,7 +56,7 @@
 						<input type="file" name="image">
 				</div>
 				<div>
-						<textarea name="text" cols="40" rows="4" placeholder="ssss"></textarea>
+						<textarea name="text" cols="40" rows="4" placeholder="Nhập ghi chú"></textarea>
 				</div>
 				<div>
 					<input type="submit" name="upload" value="Upload images">
@@ -66,20 +65,33 @@
         </div>
         </div>
         </div>
+<input type="button" id="btn" value="Xem kết quả"/><br>
+         
+        <script language="javascript">
+            document.getElementById('btn').onclick = function()
+            {
+                // Khai báo tham số
+                var checkbox = document.getElementsByName('ckId');
+                var result = "";
+                 
+                // Lặp qua từng checkbox để lấy giá trị
+                for (var i = 0; i < checkbox.length; i++){
+                    if (checkbox[i].checked === true){
+                        result += ' [' + checkbox[i].value + ']';
+                    }
+                }
+// In ra kết quả
+                alert("Các tiêu chí chưa đạt: " + result);
+            };
+        </script>
 
-    </form>
-    </div>
-    <div id="dlgP-buttons">
-        <a href="javascript:void(0)" class="easyui-linkbutton c6" iconCls="icon-ok" onclick="saveP()" style="width:90px">Save</a>
-        <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-cancel" onclick="javascript:$('#dlgP').dialog('close')" style="width:90px">Cancel</a>
-    </div>
-    
-<table id="dg"  >
-  
-    
-</table>
+    <a href="javascript:void(0)" plain = "true" id="save" class="easyui-linkbutton" data-options=" iconCls:'icon-save' " >Lưu đánh giá</a>
+    <a href="#" id="sent" plain="true" class="easyui-linkbutton" data-options="iconCls:'icon-redo', disabled:true">Chuyển cấp trên</a>
+    <a href="javascript:void(0)" plain="true" id="resent"  class="easyui-linkbutton"  data-options="iconCls:'icon-undo' ,disabled:true" >Lấy lại phiếu đã gửi</a>
+<script type="text/javascript">
+function newP() {
 
-    <script type="text/javascript">
+$('#dlgP').dialog('open').dialog('center').dialog('setTitle','Thêm kỳ phiếu');}
     $('#save').click(function(){
        $('#sent').linkbutton('enable');
        
@@ -93,18 +105,21 @@
         rownumbers:true,
         fitColumns:true, 
         nowrap: false,
-
+        singleSelect: true,
       
         idField:'maTieuchi',
-        columns:[[  {field:'tenTieuchi',title:'Tên tiêu thí',width:30}, 
-        {field:'chitietTieuchi',title:'Chi tiết tiêu chí',width:50, height:40},
-        {field:'maTieuchi',align: 'center',title:'Đánh giá chưa đạt',width:10,formatter: function(value,row,index){
+        columns:[[  {field:'tenTieuchi',title:'Tên tiêu thí',width:50}, 
+        {field:'chitietTieuchi',title:'Chi tiết tiêu chí',width:50, height:50},
+        {field:'maTieuchi',align: 'center',title:'Đánh giá',width:10,formatter: function(value,row,index){
             return row.tenTieuchi == "ly3" ? '<input type="checkbox" disabled="disabled" name="ckId" />'
             :'<input type="checkbox" name="ckId" value="'+value+'" />';
-        }}
-        
+        }},
+        {field:'action',title:'Action',width:20,align:'center',
+                formatter:function(value,row,index){
+                         var d = '<a href="javascript:void(0)" onclick="newP()" >Thêm hình ảnh</a>';
+                        return d; }}
         ]],
-        
+
         onLoadSuccess:function(){
             $("#ck_all").click(function(){
                 if($(this).attr("checked"))
@@ -136,73 +151,13 @@
             }
         },
         onUnselect:function(rowIndex, rowData){
-            $("input[value='"+rowData.maTieuchi+"']").attr("checked",false);
+$("input[value='"+rowData.maTieuchi+"']").attr("checked",false);
         }  
 
     });   
 })
 
 
-
 </script>
-<script type="text/javascript">
 
-function  editP(){
-    var row = $('#dg').datagrid('getSelected');
-    if (row){
-        $('#dlgP').dialog('open').dialog('center').dialog('setTitle','Thêm ghi chú');
-        $('#fmP').form('load',row);
-        url = 'phieudanhgia/editData.php?maTieuchi='+row.maTieuchi;
-    }
-}
-function  saveTC () {
-    $('#fmTC').form('submit',{
-        url: url,
-        onSubmit: function(){
-            return $(this).form('validate');
-        },
-        success: function(response){
-            var respData = $.parseJSON(response);
-            if(respData.status == 0){
-                $ .messager.show ({
-                    title: 'Error',
-                    msg: respData.msg
-                });
-            }else{
-                $('#dlgTC').dialog('close');
-                $('#dgTC').datagrid('reload');
-            }
-        }
-    });
-}
-
-
-
-</script>
-<input type="button" id="btn" value="Xem kết quả"/><br>
-         
-        <script language="javascript">
-            document.getElementById('btn').onclick = function()
-            {
-                // Khai báo tham số
-                var checkbox = document.getElementsByName('ckId');
-                var result = "";
-                 
-                // Lặp qua từng checkbox để lấy giá trị
-                for (var i = 0; i < checkbox.length; i++){
-                    if (checkbox[i].checked === true){
-                        result += ' [' + checkbox[i].value + ']';
-                    }
-                }
-                 
-                // In ra kết quả
-                alert("Các tiêu chí chưa đạt: " + result);
-            };
-        </script>
-    <a href="javascript:void(0)" plain = "true" id="save" class="easyui-linkbutton" data-options=" iconCls:'icon-save' " >Lưu đánh giá</a>
-    <a href="#" id="sent" plain="true" class="easyui-linkbutton" data-options="iconCls:'icon-redo', disabled:true">Chuyển cấp trên</a>
-    <a href="javascript:void(0)" plain="true" id="resent"  class="easyui-linkbutton"  data-options="iconCls:'icon-undo' ,disabled:true" >Lấy lại phiếu đã gửi</a>
-    
-</body>
 </html>
-
